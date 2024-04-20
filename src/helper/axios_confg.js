1149d0492dac8417cb2745c8c91b23371922f506
unknown <you@example.com>
2024-04-20 15:17:31 -0500

    nueva url

--- src/helper/axios_confg.js
+++ src/helper/axios_confg.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-     baseURL: 'http://localhost:4000/'
-    //baseURL: 'https://app-peliculas-bk.onrender.com/'
+     //baseURL: 'http://localhost:4000/'
+    baseURL: 'https://app-bk-i9x6.onrender.com'
 });
 
 export{
