58af2f7d1286fc240386a7fbdb24dc94d17bfd5f
unknown <you@example.com>
2024-04-20 15:28:40 -0500

    nueva

--- src/helper/axios_confg.js
+++ src/helper/axios_confg.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
      //baseURL: 'http://localhost:4000/'
-    baseURL: 'https://app-bk-i9x6.onrender.com'
+    baseURL: 'https://app-bk-i9x6.onrender.com/'
 });
 
 export{
